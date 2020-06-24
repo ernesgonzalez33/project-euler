c1da5719989922300244e0209c606ec47415c5ce
Ernesto González <100318050@alumnos.uc3m.es>
2020-06-24 11:32:31 +0200

    No idea

---
+++ CSharp/Problem2.cs
@@ -0,0 +1,40 @@
+using System;
+
+public class Problem2 
+{
+  //Recursive function to get the 3 and 5 multipliers
+  public static int Euler1(int current, int sum, int final)
+  {
+    if (current == final)
+    {
+      return sum;
+    }
+    else if (current % 3 == 0 | current % 5 == 0)
+    {
+      sum += current;
+      current++;
+      return Euler1(current, sum, final);
+    }
+    else
+    {
+      current++;
+      return Euler1(current, sum, final);
+    }
+  }
+
+  public static void Main(string[] args)
+  {
+    if (args.Length != 1)
+    {
+      Console.WriteLine("Just one argument");
+    }
+    else
+    {
+      int final = Convert.ToInt32(args[0]);
+      int sum = 0;
+      int current = 0;
+      sum = Euler1(current, sum, final);
+      Console.WriteLine(sum);
+    }
+  }
+}
